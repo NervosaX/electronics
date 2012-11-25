953ea2e90777e2d69e076da5b08e3dce02325537
Nervosa <adam@messymind.net>
2012-11-25 17:32:57 +1100

    Typo

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '   messymind_pseudo ',                      # Or path to database file if using sqlite3.
+        'NAME': 'messymind_pseudo ',                      # Or path to database file if using sqlite3.
         'USER': 'messymind_pseudo',                      # Not used with sqlite3.
         'PASSWORD': '024e9ec5',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
