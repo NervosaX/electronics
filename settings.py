5acf4f401e03145d34052e9d55f150a4595e0bdf
Nervosa <adam@messymind.net>
2012-11-25 17:40:02 +1100

    Adding static directory

--- settings.py
+++ settings.py
@@ -5,8 +5,6 @@ import sys
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 PROJECT_DIR = os.path.dirname(__file__).split('/')[-1]
 
-
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -63,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/messymind/webapps/pseudo_static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
